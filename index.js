545f4c0eb38a597957b00d4daaf2aa8f45638f67
hihebark <n.amara@protonmail.ch>
2020-11-16 11:20:43 +0100

    notify me only about specific branches...

--- index.js
+++ index.js
@@ -7,23 +7,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/healthz', (req, res) => {res.status(204).send()});
 
 app.use('/callback/github', (req, res) => {
+  let allowedBranch = ['develop', 'feedback'];
   let repository = req.body.repository.name
   , commits = req.body.commits
   , ref = req.body.ref;
   const title = '*Backend* ('+repository+'): _['+ref.substring(ref.lastIndexOf('/') + 1)+']_ pushed '+process.env.SUPERVISOR+'\n';
   const message_commited = 'Commited:\n- ' + commits.filter(v => v.author.name == 'hihebark').map(v => v.message).join('\n- ');
-
-  const axios = require('axios');
-  axios.post(
-    process.env.ME_CHANNEL,
-    { text: title+message_commited, type: 'mrkdwn' }
-  ).then((response) => {
-    if (response.data == 'ok')
-      console.log('[!] Message sent...');
-    else console.log('[!] Message not sent...');
-  }).catch((err) => {
-    console.log(err);
-  });
+  if (allowedBranch.includes(ref.substring(ref.lastIndexOf('/') + 1))) {
+    const axios = require('axios');
+    axios.post(
+      process.env.ME_CHANNEL,
+      { text: title+message_commited, type: 'mrkdwn' }
+    ).then((response) => {
+      if (response.data == 'ok')
+        console.log('[!] Message sent...');
+      else console.log('[!] Message not sent...');
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
   return res.status(200).send({});
 });
 
